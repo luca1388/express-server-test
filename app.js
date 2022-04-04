06343249fbc8e8d05d5e7b0f5122229f96146e14
Luca Gandini <lgandini@funambol.com>
2022-04-04 15:04:40 +0200

    Logs

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-require("dotenv").config(); // load .env variables
-const express = require("express"); // import express
-const morgan = require("morgan"); //import morgan
-const { log } = require("mercedlogger"); // import mercedlogger's log function
-const cors = require("cors"); // import cors
+// load .env variables
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const { log } = require("mercedlogger");
+const cors = require("cors");
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 // Middleware
-app.use(cors()); // add cors headers
+app.use(cors());
 app.use(morgan("tiny")); // log the request for debugging
-app.use(express.json()); // parse json bodies
+app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("this is the test route to make sure server is working");
+  res.json({ status: "ok" });
 });
 
 app.listen(PORT, () => log.green("SERVER STATUS", `Listening on port ${PORT}`));
